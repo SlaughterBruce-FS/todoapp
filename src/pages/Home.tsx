3c0c2e20338ae7bbce95d1b6b7470e9adc1a7bdf
bruce.slaughter <BLSlaughter1@student.fullsail.edu>
2023-01-31 22:29:02 -0500

    update app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonButtons, IonIcon, IonMenuButton, IonFab,IonFabButton, IonModal } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonButtons, IonIcon, IonMenuButton, IonFab,IonFabButton, IonModal, IonRow, IonCol, IonItem, IonInput, IonTextarea } from '@ionic/react';
 
 // import Featured from '../components/Featured';
 import axios, { AxiosResponse } from 'axios';
 import { useState, useEffect } from "react";
 import React from 'react';
 import { add } from 'ionicons/icons';
+import { IonDatetime } from '@ionic/react';
 
 
 
@@ -12,6 +13,9 @@ const Home: React.FC = () => {
 
 
   const [sheetOpen, setSheet] = useState(false)
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
 
   const [name, setName] = useState('');
 
@@ -38,6 +42,40 @@ const Home: React.FC = () => {
             <IonIcon icon={add} />
           </IonFabButton>
         </IonFab>
+
+        <IonModal
+         isOpen={sheetOpen}
+         onDidDismiss={() => setSheet(false)}
+         breakpoints={[0, 0.7]}
+         initialBreakpoint={0.7}
+         >
+          <IonContent>
+            <IonRow>
+              <IonCol size='12'>
+                <IonItem lines='full'>
+                  <IonInput 
+                  placeholder='Todo Name'
+                  value={title}
+                  onIonChange={(e: CustomEvent) => setTitle(e.detail.value)}
+                   />
+                </IonItem>
+              </IonCol>
+              <IonCol size='12'>
+                <IonItem lines='full'>
+                <IonTextarea 
+                placeholder='Description'
+                rows={2}
+                value={description}
+                onIonChange={(e: CustomEvent) => setDescription(e.detail.value)}
+                />
+                </IonItem>
+              </IonCol>
+              <IonCol>
+                <IonDatetime />
+              </IonCol>
+            </IonRow>
+          </IonContent>
+        </IonModal>
       </IonContent>
     </IonPage>
   );
